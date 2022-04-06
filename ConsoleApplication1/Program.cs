24f3fd5ab5eb814a4d68249da4abd3bfbdf85be8
ZahraTavakoli <100377681+ZAHRATAVAKoLE@users.noreply.github.com>
2022-04-07 03:07:16 +0430

    Add files via upload

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,9 +10,10 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Random r = new Random();
-            int user_score = 0, computer_score = 0, index;
-            string computer_choice, user_choice;
+            Random r1 = new Random();
+            Random r2 = new Random();
+            int user_score = 0, computer1_score = 0, computer2_score = 0 ,index1,index2;
+            string computer1_choice, computer2_choice,user_choice;
             string[] options = new string[2];
             
             options[0] = "on";
@@ -20,30 +21,39 @@ namespace ConsoleApplication1
 
             while (true)
             {
-                index = r.Next(0, 2);
-                computer_choice = options[index];
+                index1 = r1.Next(0, 2);
+                computer1_choice = options[index1];
+                index2 = r2.Next(0, 2);
+                computer2_choice = options[index2];
                 user_choice = Console.ReadLine();
-                Console.WriteLine(computer_choice);
-                if (computer_choice == "on" && user_choice == "back")
+                Console.WriteLine(computer1_choice);
+                Console.WriteLine(computer2_choice);
+                if (computer1_choice == "on" && user_choice == "back"&& computer2_choice == "back")
+
                 {
-                    computer_score++;
+                    computer1_score++;
                 }
 
-                else if (computer_choice == "back" && user_choice == "on")
+                else if (computer1_choice == "back" && user_choice == "on" && computer2_choice == "back" )
                 {
                     user_score++;
                 }
 
-                if (computer_score ==5 || user_score==5)
+                else if (computer1_choice == "back" && user_choice == "back" && computer2_choice == "on")
+                {
+                    computer2_score++;
+                }
+               
+                if (computer1_score ==5 || user_score==5 ||computer2_score ==5 )
                     
                 { 
                     break;
                 }
 
             }
-            if (computer_score == 5)
+            if (computer_score1 == 5)
             {
-                Console.WriteLine("COMPUTER WIN ");
+                Console.WriteLine("COMPUTER1 WIN ");
                
             }
             else if (user_score == 5)
@@ -51,7 +61,11 @@ namespace ConsoleApplication1
                 Console.WriteLine(" YOU WIN");
                 
             }
-
+            else if (computer_score2 == 5)
+            {
+                Console.WriteLine("COMPUTER2 WIN ");
+            }  
+           
             
             
        
